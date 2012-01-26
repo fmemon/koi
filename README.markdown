d7e29af1dca9788466e6b5665fa6e86b89cb70d4
Your Name <fmemon100@yahoo.com>
2012-01-26 15:12:53 -0500

    First commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Ruby on Rails: my first application Koi
+
+This is the first application for
+[*to demonstrate my rails skills*](http://herokuapp.com/)
+by [Farida Memon](http://bestwhich.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koi::Application.config.secret_token = '39cd96f698b1227802273f1f4ff911555c85575b145f8db8c1d5775d47fe531ddc85932c75c994d732e582c726b912fcd405a6221b612094fa71797c2ad98d1f'
